420b28efa547fcc28ab494417ca3e4c28c495b66
rawaha-coder <rawaha.coder@gmail.com>
2021-08-10 23:15:33 +0100

    LayoutCodelab first part done

--- LayoutsCodelab/app/src/main/java/com/rawahastudio/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/com/rawahastudio/layoutscodelab/MainActivity.kt
@@ -3,70 +3,205 @@ package com.rawahastudio.layoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.rawahastudio.layoutscodelab.ui.theme.LayoutsCodelabTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhotographerCard()
-        }
-    }
-}
-
-@Composable
-fun PhotographerCard(modifier: Modifier = Modifier) {
-    Row(modifier
-        .padding(8.dp)
-        .clip(RoundedCornerShape(4.dp))
-        .background(MaterialTheme.colors.surface)
-        .clickable(onClick = { /* Ignoring onClick */ })
-        .padding(16.dp)
-
-    ) {
-        Surface(modifier = Modifier.size(50.dp),
-            shape = CircleShape,
-        color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ) {
-            // Image goes here
-
-        }
-        Column(modifier = Modifier
-            .padding(start = 8.dp)
-            .align(Alignment.CenterVertically)
-        ) {
-            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-            // LocalContentAlpha is defining opacity level of its children
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            LayoutsCodelabTheme {
+                LayoutsCodelab()
             }
         }
     }
-
 }
+
+
+@Composable
+fun LayoutsCodelab(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                Text(text = "LayoutsCodelab")
+            },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Add, contentDescription = null)
+                    }
+                }
+            )
+        }, bottomBar = {
+            var selectedItem by remember { mutableStateOf(0) }
+            val items = listOf("Songs", "Artists", "Playlists")
+
+            BottomNavigation {
+                items.forEachIndexed { index, item ->
+                    BottomNavigationItem(
+                        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                        label = { Text(item) },
+                        selected = selectedItem == index,
+                        onClick = { selectedItem = index }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        BodyContent(modifier = Modifier.padding(innerPadding).padding(8.dp))
+    }
+}
+
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier){
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+        Divider(modifier.padding(top = 8.dp, bottom = 1.dp), color = Color.Green, thickness = 2.dp)
+        LazyList()
+    }
+}
+
+@Composable
+fun LazyList() {
+    val listSize = 100
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }, modifier = Modifier.padding(4.dp)) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }, modifier = Modifier.padding(4.dp)) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+
+
+
+
+@Composable
+fun SimpleList() {
+    // We save the scrolling position with this state that can also
+    // be used to programmatically scroll the list
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #$it")
+        }
+    }
+}
+
 
 @Preview
 @Composable
-fun PhotographerCardPreview() {
+fun LayoutsCodelabPreview() {
     LayoutsCodelabTheme {
-        PhotographerCard()
+        LayoutsCodelab()
     }
 }
+
+
+//@Composable
+//fun PhotographerCard(modifier: Modifier = Modifier) {
+//    Row(modifier
+//        .padding(8.dp)
+//        .clip(RoundedCornerShape(4.dp))
+//        .background(MaterialTheme.colors.surface)
+//        .clickable(onClick = { /* Ignoring onClick */ })
+//        .padding(16.dp)
+//
+//    ) {
+//        Surface(modifier = Modifier.size(50.dp),
+//            shape = CircleShape,
+//        color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+//        ) {
+//            // Image goes here
+//
+//        }
+//        Column(modifier = Modifier
+//            .padding(start = 8.dp)
+//            .align(Alignment.CenterVertically)
+//        ) {
+//            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+//            // LocalContentAlpha is defining opacity level of its children
+//            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+//                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+//            }
+//        }
+//    }
+//
+//}
+//
+//@Preview
+//@Composable
+//fun PhotographerCardPreview() {
+//    LayoutsCodelabTheme {
+//        PhotographerCard()
+//    }
+//}
